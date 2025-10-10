a05eefcb75b89abd9c92f3eb64ceda2e1de5927e
25StephenKellyHannon <25StephenKellyHannon@LC.local>
2025-10-10 09:50:35 +0100

    More tests again

---
+++ FinancialReportApp.Tests/Util/UIRegistryTests.cs
@@ -0,0 +1,89 @@
+﻿using FinancialReportApp.UI;
+using FinancialReportApp.UI.Menus;
+using FinancialReportApp.UI.UIs;
+using FinancialReportApp.Util;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialReportApp.Tests.Util
+{
+    [TestClass]
+    public class UIRegistryTests
+    {
+        UIRegistry registry;
+
+        Mock<IServiceProvider> mockProvider;
+        Mock<IUIFlowController> mockFlowController;
+        Mock<IUserInterface> mockUserInterface;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            mockProvider = new Mock<IServiceProvider>();
+            mockFlowController = new Mock<IUIFlowController>();
+            mockUserInterface = new Mock<IUserInterface>();
+
+            registry = new UIRegistry(mockProvider.Object, mockFlowController.Object);
+        }
+
+        [TestMethod]
+        public void BuildMenuHierarchy_MenusRegistered_CorrectHierarchyBuilt()
+        {
+            var rootMenu = new TestMenu(mockUserInterface.Object);
+            var childMenu = new TestMenu(mockUserInterface.Object);
+            var dummyScreen = new TestUI(mockUserInterface.Object);
+
+            mockProvider.Setup(p => p.GetService(typeof(TestMenu))).Returns(rootMenu);
+            mockProvider.Setup(p => p.GetService(typeof(TestUI))).Returns(dummyScreen);
+            mockProvider.Setup(p => p.GetService(typeof(TestMenuChild))).Returns(childMenu);
+
+            registry.RegisterMenu("Root Menu", typeof(TestMenu));                 // root
+            registry.RegisterMenu("Child Menu", typeof(TestUI), typeof(TestMenu)); // child
+            registry.RegisterMenu("SubMenu", typeof(TestMenuChild), typeof(TestMenu));  // child menu
+            registry.BuildMenuHierarchy();
+
+            Assert.IsTrue(rootMenu.GetActionLabels().Contains("Exit"), "Root menu should contain Exit action.");
+            Assert.IsTrue(childMenu.GetActionLabels().Contains("Back"), "Child menu should contain Back action.");
+            Assert.IsTrue(rootMenu.GetActionLabels().Contains("Child Menu"), "Root should have child item label added.");
+        }
+    }
+
+    public class TestMenu : Menu, IDisplayableUI
+    {
+        public List<string> AddedActions { get; } 
+
+        public TestMenu(IUserInterface ui) : base(ui, "start", "end", "error") { }
+
+        public override void Display() { }
+
+        public new void AddMenuAction(string label, Action action, int? order = null)
+        {
+            AddedActions.Add(label);
+            base.AddMenuAction(label, action, order);
+        }
+
+        public IEnumerable<string> GetActionLabels() => menuActions.Values.Select(v => v.label);
+    }
+
+    public class TestMenuChild : Menu, IDisplayableUI
+    {
+        public TestMenuChild(IUserInterface userInterface, string startText, string endText, string errorText) : base(userInterface, startText, endText, errorText)
+        {
+        }
+    }
+
+    public class TestUI : UIBase, IDisplayableUI
+    {
+        public TestUI(IUserInterface ui) : base(ui) { }
+
+        public override void Display()
+        {
+
+        }
+    }
+}
---
+++ FinancialReportApp.Tests/Util/UserDataTests.cs
@@ -0,0 +1,70 @@
+﻿using FinancialReportApp.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialReportApp.Tests.Util
+{
+    [TestClass]
+    public class UserDataTests
+    {
+        [TestMethod]
+        public void ClearAllData_ShouldResetAllProperties()
+        {
+            // Arrange
+            var userData = new UserData
+            {
+                Salary = 50000m,
+                IsSalaryBeforeTax = false,
+                TaxPaid = 10000m,
+                SalaryFrequency = TimeFrequency.Yearly,
+                UseCustomTax = true
+            };
+
+            userData.AddExpense("Rent", 1000m, TimeFrequency.Monthly);
+            userData.AddTaxBracket(0m, 35000m, 0.2m);
+            userData.AddTaxCredit(1650m);
+            
+            userData.ClearAllData();
+            
+            Assert.AreEqual(0m, userData.Salary);
+            Assert.IsTrue(userData.IsSalaryBeforeTax);
+            Assert.AreEqual(0m, userData.TaxPaid);
+            Assert.AreEqual(default(TimeFrequency), userData.SalaryFrequency);
+            Assert.AreEqual(0, userData.Expenses.Count);
+            Assert.IsFalse(userData.UseCustomTax);
+            Assert.AreEqual(0, userData.CustomTaxBrackets.Count);
+            Assert.AreEqual(0, userData.TaxCredits.Count);
+        }
+
+        [TestMethod]
+        public void AddAndRemoveExpense_ShouldModifyExpensesList()
+        {
+            var userData = new UserData();
+            userData.AddExpense("Groceries", 200m, TimeFrequency.Weekly);
+            userData.AddExpense("Utilities", 150m, TimeFrequency.Monthly);
+            
+            userData.RemoveExpense(0); 
+            
+            Assert.AreEqual(1, userData.Expenses.Count);
+            Assert.AreEqual("Utilities", userData.Expenses[0].Category);
+        }
+
+        [TestMethod]
+        public void AddAndRemoveTaxCredits_ShouldModifyCustomTaxCreditsList()
+        {
+            var userData = new UserData();
+            userData.AddTaxCredit(500m);
+            userData.AddTaxCredit(1000m);
+
+            Assert.AreEqual(2, userData.TaxCredits.Count);
+
+            userData.RemoveTaxCredit(0);
+            
+            Assert.AreEqual(1, userData.TaxCredits.Count);
+            Assert.AreEqual(1000m, userData.TaxCredits[0]);
+        }
+    }
+}
